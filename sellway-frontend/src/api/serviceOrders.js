b410fb0fc2a7439919fe22c32ae232e470821275
Виктор <78488229+viktor138irk@users.noreply.github.com>
2026-05-24 02:53:11 +0900

    Добавил клиент API услуг

---
+++ sellway-frontend/src/api/serviceOrders.js
@@ -0,0 +1,5 @@
+import client from './client';
+
+export const createServiceOrder = (product_id, message = '') => client.post('/service-orders', { product_id, message });
+export const sendServiceProposal = (id, data) => client.post(`/service-orders/${id}/proposal`, data);
+export const acceptServiceProposal = (id) => client.post(`/service-orders/${id}/accept-proposal`);
